c526a3615386543740e0e516ee1ca78837f7c631
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-04 19:47:20 +0800

    New

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,3 +1,31 @@
-﻿var True = false;
+﻿class House{
 
-Console.WriteLine(True);
+    string color;
+    string owningCompany;
+    int numberOfWindows;
+    string houseID;
+    bool available = true;
+    public void status(){
+        Console.WriteLine("The house {0} is available", houseID); 
+        
+    }
+    
+
+    public static void Main(){
+        House myhome = new House();
+        
+        myhome.color = "Blue";
+        myhome.owningCompany = "Bridge Homes";
+        myhome.numberOfWindows = 4;
+        myhome.houseID = "ACB 32";
+
+        myhome.status();
+
+        if(myhome.available == true){
+            Console.WriteLine("When can we move in?");
+        } 
+
+        Console.Write("The house is owned by {0}. ", myhome.owningCompany);
+        Console.WriteLine("House Specifications include: {0}\n{1}\n", myhome.color, myhome.numberOfWindows);
+    }
+}
