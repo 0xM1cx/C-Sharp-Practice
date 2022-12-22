dcb6510f31c0d1076f0944f760158b462b9cabcd
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-22 23:46:04 +0800

    Learned Jagged Array

--- DayOne/Program.cs
+++ DayOne/Program.cs
@@ -17,10 +17,33 @@ foreach (int age in agesOfStudents)
 
 // Multidimensional Arrays
 
-
+Console.WriteLine();
 int[,] nums = {{14, 13, 41}, {14, 123, 13}, {10, 31, 4}};
 
-foreach (int item in nums)
+// foreach (int item in nums)
+// {
+//     Console.WriteLine("\n{0}", item);
+// }
+
+Console.WriteLine(nums[1,0]); // 14
+Console.WriteLine(nums[0,1]); // 13
+Console.WriteLine(nums[2,0]); // 10
+Console.WriteLine(nums[2,1]); // 31
+
+
+// Jagged Array
+
+string[][] names = {
+    new string[] {"SHAWN", "MICHAEL", "SUDARIA"},
+    new string[] {"CRYSTAL", "NIKKI", "PATRICIA", "JOHN", "JESSICA"}
+};
+
+for (int i = 0; i < names.Length; i++)
 {
-    Console.WriteLine("\n{0}", item);
+    for(int j = 0; j < names[i].Length; j++)
+    {
+        Console.WriteLine(names[i][j]);
+    }
 }
+
+Console.WriteLine("My name is {0} {1} {2}", names[0][0], names[0][1], names[0][2]);
