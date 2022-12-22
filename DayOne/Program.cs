216c8163f40ddfe45b2ebbf9b81ec617c07f8388
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-22 22:25:02 +0800

    update

--- DayOne/Program.cs
+++ DayOne/Program.cs
@@ -6,3 +6,10 @@ int userInput = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("{0}", userInput);
 
+// Arrays Practice
+int[] agesOfStudents = {17, 18, 19, 20, 17, 18, 20, 19};
+
+foreach (int age in agesOfStudents)
+{
+    Console.WriteLine(age);
+}
