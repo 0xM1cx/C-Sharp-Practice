0444ac31a34958aa0c7ae44d8d13e007cbe5df75
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-24 16:33:31 +0800

    Access Modifier Practic

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,31 +1,55 @@
-﻿class House{
+﻿using System;
 
-    string color;
-    string owningCompany;
-    int numberOfWindows;
-    string houseID;
-    bool available = true;
-    public void status(){
-        Console.WriteLine("The house {0} is available", houseID); 
-        
-    }
+namespace ClassPractice
+{
     
+    class House
+    {
 
-    public static void Main(){
-        House myhome = new House();
+        public string color;
+        public string owningCompany;
+        public int numberOfWindows;
+        public string houseID;
+        public bool available = true;
+        public void status(){
+            Console.WriteLine("The house {0} is available", houseID); 
+            
+        }
         
-        myhome.color = "Blue";
-        myhome.owningCompany = "Bridge Homes";
-        myhome.numberOfWindows = 4;
-        myhome.houseID = "ACB 32";
 
-        myhome.status();
+        static void Main(string[] args)
+        {
+            House myhome = new House();
+            
+            myhome.color = "Blue";
+            myhome.owningCompany = "Bridge Homes";
+            myhome.numberOfWindows = 4;
+            myhome.houseID = "ACB 32";
 
-        if(myhome.available == true){
-            Console.WriteLine("When can we move in?");
-        } 
+            myhome.status();
 
-        Console.Write("The house is owned by {0}. ", myhome.owningCompany);
-        Console.WriteLine("House Specifications include: {0}\n{1}\n", myhome.color, myhome.numberOfWindows);
+            // if(myhome.available == true){
+            //     Console.WriteLine("When can we move in?");
+            // } 
+
+            // Console.Write("The house is owned by {0}. ", myhome.owningCompany);
+            // Console.WriteLine("House Specifications include: {0}\n{1}\n", myhome.color, myhome.numberOfWindows);
+        }
+
+
+
     }
+
+    class exHouse 
+    {
+        public void akonBalay(){
+            House h1 = new House();
+            h1.owningCompany = "Camella Homes";
+            Console.WriteLine(h1.owningCompany);
+        }
+
+        akonBalay();
+
+    }
+
 }
