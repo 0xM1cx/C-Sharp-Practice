4c05babb3ffa913f0e9dbb3039bc6a9d78904916
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-25 11:50:10 +0800

    this keyword practice

---
+++ DayFive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConstructorPractice
+{
+    class Test
+    {
+        string name = "Shawn";
+
+        Test(string nickname)
+        {
+            Console.WriteLine("Before assigning a new value: {0}", this.name);
+            this.name = nickname;
+            Console.WriteLine("The object of this:  {0}", this.name);
+        }
+
+        static void Main(string[] args)
+        {
+            Test test = new Test("Shun");
+            Console.WriteLine("The object of test:  {0}",test.name);
+        }
+    }
+}
