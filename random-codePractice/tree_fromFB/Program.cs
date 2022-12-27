dea4ff371464a9a1fce824bcb0db8ceb66aa1436
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-27 18:37:19 +0800

    Random Challenge from FB group

---
+++ random-codePractice/tree_fromFB/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tree
+{
+    class Tree
+    {
+
+
+        public void NumberSquare()
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                for(int j = 5; j >= i; j--)
+                {
+                    Console.Write(j);
+                }
+                for(int b = 1; b < i; b++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.NumberSquare();
+        }
+    }
+}
