dc3282be816b156afa8ab1e7af73dcdc60f3caf9
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-29 17:54:09 +0800

    Completed Day 2 part 2 of advent of code

--- advent-of-code/RockPaperScissors/Program.cs
+++ advent-of-code/RockPaperScissors/Program.cs
@@ -10,7 +10,7 @@ namespace MainProgram
     class RPS
     {
 
-        public int Score(string[] currentRound)
+        public int ScorePartOne(string[] currentRound)
         {
             // This function deals will the game.
             // Like when the user has X(Rock) and the enemy draws A(Rock) it will be a draw, returning 3 as stated in the rules.
@@ -47,10 +47,8 @@ namespace MainProgram
             return 0;
         }
 
-
-        public static void Main(string[] args)
+        public void PartOne()
         {
-
             List<string> choices = new List<string>(); // Declared a list for the possible player choices.
             
             string[] lines = File.ReadAllLines(".\\input.txt"); // Get the input from the input.txt file
@@ -69,7 +67,7 @@ namespace MainProgram
             foreach (string line in lines) // This is loop through every line in tne input.txt file
             {
                 currentRound = line.Split(' '); // For every line the File it will split the string into 2, these are strings separted by space
-                int roundScore = rps.Score(currentRound); // This invoked the Score() and return an integer score, depending on the values of the user and the enemy
+                int roundScore = rps.ScorePartOne(currentRound); // This invoked the Score() and return an integer score, depending on the values of the user and the enemy
                 
 
                 // This determined the index for the player input, because this will be used as added points later on.
@@ -81,8 +79,120 @@ namespace MainProgram
 
 
             Console.WriteLine("The Total Score is {0}", Total);            // Outputs the total score of all the rounds
+        }
+
+
+
+        // ===== This is part 2 of the challege, this was given after I have solved part 1 =====
+        
+        public string ReturnShape = " ";
+
+        public int ScorePartTwo(string[] currentRound)
+        {
+            // This function deals will the game
+            // Like when the user has X(Rock) and the enemy draws A(Rock) it will be a draw, returning 3 as stated in the rules.
+            RPS rps = new RPS();
+            
+            if(currentRound[1] == "Y")
+            {
+                if(currentRound[0] == "A")
+                {
+                    ReturnShape = "X";
+                    return 3;        
+                }
+                else if(currentRound[0] == "B")
+                {
+                    ReturnShape = "Y";
+                    return 3;        
+                }
+                else if(currentRound[0] == "C")
+                {
+                    ReturnShape = "Z";   
+                    return 3;     
+                }
+            }
+            else if(currentRound[1] == "X")
+            {
+                if(currentRound[0] == "A")
+                {
+                    ReturnShape = "Z";
+                    return 0;        
+                }
+                else if(currentRound[0] == "B")
+                {
+                    ReturnShape = "X";  
+                    return 0;      
+                }
+                else if(currentRound[0] == "C")
+                {
+                    ReturnShape = "Y";    
+                    return 0;    
+                }
+            }
+            else if(currentRound[1] == "Z")
+            {
+                if(currentRound[0] == "A")
+                {
+                    ReturnShape = "Y";
+                    return 6;        
+                }
+                else if(currentRound[0] == "B")
+                {
+                    ReturnShape = "Z";
+                    return 6;        
+                }
+                else if(currentRound[0] == "C")
+                {
+                    ReturnShape = "X";
+                    return 6;
+                }
+            }
+
+            return 0;
+        }
+
+
+        public void PartTwo()
+        {
+            List<string> choices = new List<string>(); // Declared a list for the possible player choices.
+            
+            string[] lines = File.ReadAllLines(".\\input.txt"); // Get the input from the input.txt file
+            string[] currentRound = new string[2]; // For every 2 letters, like A X, in every line in the file, it will be stored here
 
             
+            //  These will be the possible values for the users to have. 
+            // X is Rock, Y is Paper and Z is Scissors
+            choices.Add("X"); 
+            choices.Add("Y");
+            choices.Add("Z");
+            
+            RPS rps = new RPS(); // made an object so that I can access the Score() function
+            int Total = 0; // the total score from all the rounds/lines
+
+            foreach (string line in lines) // This is loop through every line in tne input.txt file
+            {
+                currentRound = line.Split(' '); // For every line the File it will split the string into 2, these are strings separted by space
+                int roundScore = rps.ScorePartTwo(currentRound); // This invoked the Score() and return an integer score, depending on the values of the user and the enemy
+                
+
+                // This determined the index for the player input, because this will be used as added points later on.
+                // Using X will give additional 1 point, Y will be 2 points and Z will be 3 points.
+                int index = choices.FindIndex(a => a.Contains(rps.ReturnShape)); 
+            
+                Total += roundScore + (index + 1); // This adds the score the player got depending in the comparison made and the index for which option the player used 
+            
+            }
+
+
+
+            Console.WriteLine("The Total Score is {0}", Total);    
+        }
+
+        public static void Main(string[] args)
+        {
+            RPS rps = new RPS();
+
+            rps.PartTwo();
         }
     }
 }
