0d7f3453f3b64c78e9030e7ebc5a965b0a964f41
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-31 16:05:03 +0800

    Updated Code & Fixed Errors on if duplicates exist

--- advent-of-code/Rucksack-Reorganization/Program.cs
+++ advent-of-code/Rucksack-Reorganization/Program.cs
@@ -6,17 +6,34 @@ namespace Program
 {
     class RucksackReorganization 
     {
+
+        public int Total = 0;
+        public int CalculatePriorityPoints(List<char> item_dups, string letters)
+        {
+            
+            foreach (char dup in item_dups)
+            {
+                Total += letters.IndexOf(dup) + 1;
+            }
+            return Total;
+        }
+
         public static void Main(string[] args)
         {
             string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string[] lines = File.ReadAllLines(".\\input.txt");
+            List<char> item_duplicate = new List<char>();
+            RucksackReorganization RuckReorg = new RucksackReorganization();
+            int TotalPoints = 0;
+            int lineLength = 0;
+            List<char> firstPart = new List<char>(); 
+            List<char> secondPart = new List<char>();
 
             foreach (string line in lines)
             {
-                int lineLength = line.Length;
+                // Getting the line length for every line in the file
+                lineLength = line.Length;
                 
-                List<char> firstPart = new List<char>(); 
-                List<char> secondPart = new List<char>();
                 // Append the first part of the array to another array
                 for(int i = 0; i < (lineLength/2); i++)
                 {
@@ -26,12 +43,24 @@ namespace Program
                 // Appnding the second half of char of line to the secondPart List
                 for(int i = (lineLength/2); i < lineLength; i++)
                 {
-                    secondPart.Add(line(i));
+                    secondPart.Add(line[i]);
                 }
 
                 // Comparing the items in both list to see which is char is shared
+                foreach (char item in firstPart)
+                {
+                    if(secondPart.Contains(item))
+                    {
+                        item_duplicate.Add(item);
+                    }
+                }
+                
                 
             }
+            // Get the total priority points from all the duplicate items
+            TotalPoints = RuckReorg.CalculatePriorityPoints(item_duplicate, letters);
+            // Displaying the total priority points of the items
+            Console.WriteLine("The total priority of all the duplicates is {0}", TotalPoints);
 
         }
     }
