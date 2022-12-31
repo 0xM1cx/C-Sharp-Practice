2c87e3ac743ac30c77768426101cb1a8c9b86b63
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-31 15:29:33 +0800

    Update

--- advent-of-code/Rucksack-Reorganization/Program.cs
+++ advent-of-code/Rucksack-Reorganization/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Program
 {
@@ -7,7 +8,30 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            strings letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string[] lines = File.ReadAllLines(".\\input.txt");
+
+            foreach (string line in lines)
+            {
+                int lineLength = line.Length;
+                
+                List<char> firstPart = new List<char>(); 
+                List<char> secondPart = new List<char>();
+                // Append the first part of the array to another array
+                for(int i = 0; i < (lineLength/2); i++)
+                {
+                    firstPart.Add(line[i]);
+                }
+
+                // Appnding the second half of char of line to the secondPart List
+                for(int i = (lineLength/2); i < lineLength; i++)
+                {
+                    secondPart.Add(line(i));
+                }
+
+                // Comparing the items in both list to see which is char is shared
+                
+            }
 
         }
     }
