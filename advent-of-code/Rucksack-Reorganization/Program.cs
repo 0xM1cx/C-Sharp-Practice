8098fb2bf0a2622151e3166c96493adf38cfdf9d
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-31 13:22:25 +0800

    New Challenge | Day 3 Advent of Code

---
+++ advent-of-code/Rucksack-Reorganization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Program
+{
+    class RucksackReorganization 
+    {
+        public static void Main(string[] args)
+        {
+            strings letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        }
+    }
+}
