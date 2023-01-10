54fce180dffadad8e71821bb4d0d36cd02a59e52
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-10 18:04:58 +0800

    New

---
+++ DaySix/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            var arr = from i in Enumerable.Range(10) select number;
+            Console.WriteLine(arr);
+        }
+    }
+}
---
+++ random-codePractice/playSound/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Media;  
+
+namespace Program
+{
+    class PlayMusic
+    {
+        public static void Main(string[] args)
+        {
+            // Make an instance of the SoundPlayer Class
+            SoundPlayer musicPlayer = new SoundPlayer();
+
+            musicPlayer.SoundLocation  = AppDomain.CurrentDomain.BaseDirectory + ".\\Pixel_Music.mp3";
+            musicPlayer.Play();
+        }
+    }
+}
