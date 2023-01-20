9e2e0f45e0977f2d23fb29e2cbe749cd645ee868
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-20 15:17:33 +0800

    New Completed Challenge from CodeChef

---
+++ random-codePractice/SquatsCodeChef/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+   class Sample
+   {
+        public static void Main()
+        {
+            int testCases = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+               int squats = Int32.Parse(Console.ReadLine());
+               Console.WriteLine(squats * 15); 
+            }
+        }
+   } 
+}
