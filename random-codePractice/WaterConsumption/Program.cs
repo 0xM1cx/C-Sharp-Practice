a8320f70a92a685923948189ea2f8a42c26b6a81
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-20 15:38:10 +0800

    Updated

--- random-codePractice/WaterConsumption/Program.cs
+++ random-codePractice/WaterConsumption/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WaterConsumtion
+{
+   class Program
+   {
+        public static void Main()
+        {
+            int testCases = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+               int input = Int32.Parse(Console.ReadLine());
+               if (input >= 2000)
+               {
+                    Console.WriteLine("YES");
+               }
+               else
+               {
+                    Console.WriteLine("NO");
+               } 
+            }
+        }
+   } 
+}
