4aaf28d9f13700f6bc18387e6425d2d956735ecc
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-25 08:56:38 +0800

    Update: Completed Counting Words Problem | CodeChef

--- DayEight/Program.cs
+++ DayEight/Program.cs
@@ -11,6 +11,12 @@ namespace DayEight
             was bored, he decided to count the number of words in the book.`*/
 
            int testCase = Int32.Parse(Console.ReadLine());
+           for(int x = 1; x <= testCase; x++)
+           {
+            string[] uInput = Console.ReadLine().Split();
+            Console.WriteLine(Int32.Parse(uInput[0]) * Int32.Parse(uInput[1]));
+
+           }
         }
         public static void Main() 
         {
