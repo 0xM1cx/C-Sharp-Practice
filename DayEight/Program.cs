fc5d92e3ac2aefe878ab9ca06e53b8fce1e11f26
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-25 08:51:17 +0800

    Update: New Challenge -> Counting Words

--- DayEight/Program.cs
+++ DayEight/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DayEight
+{
+   class Program
+   {
+        static void CountingWords()
+        {
+            /*Harsh was recently gifted a book consisting of N pages. 
+            Each page contains exactly M words printed on it. As he
+            was bored, he decided to count the number of words in the book.`*/
+
+           int testCase = Int32.Parse(Console.ReadLine());
+        }
+        public static void Main() 
+        {
+            CountingWords();
+        }
+   } 
+}
